98fe11391ca7fa9d72595734a9610816ba4773c7
brandon <brandon@stylograph.net>
2009-01-11 08:42:46 +0000

    Initial commit

---
+++ app/src/de/georg_gruetter/xhsi/POC.java
@@ -0,0 +1,12 @@
+package de.georg_gruetter.xhsi;
+
+import java.util.logging.Logger;
+
+public class POC {
+	
+	public static Logger logger = Logger.getLogger("de.georg_gruetter.xhsi");
+	
+	public static void main(String args[]) throws Exception {
+	}
+
+}
